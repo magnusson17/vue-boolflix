44d5353a7a240577373c3474245d6aee95fc1fbb
Ga <gabri.magni.97@gmail.com>
2022-04-22 18:15:51 +0200

    api in axios

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,19 @@ import HeaderComp from './components/HeaderComp.vue'
 export default {
   name: 'App',
   components: {
-    HeaderComp
+    HeaderComp,
+  },
+
+  data() {
+    return {
+      api_key: '',
+    }
   }
+
+  // created: {
+  //   axios.get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut
+  //   uro")
+  // }
 }
 </script>
 
