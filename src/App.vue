b14ebbd1582298c70b2c58c6fce4c5ef2ac546f9
Ga <gabri.magni.97@gmail.com>
2022-04-22 17:19:58 +0200

    header draft

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp/>
   </div>
 </template>
 
 <script>
 import "bootstrap"
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "bootstrap/dist/css/bootstrap.min.css";
+
 </style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,2 +1,22 @@
 <template>
+    <div class="d-flex justify-content-between align-items-center p-4">
+        <div>
+            <img src="" alt="img">
+        </div>
+
+        <div>
+            <input type="text">
+            <button>cerca</button>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'HeaderComp'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
